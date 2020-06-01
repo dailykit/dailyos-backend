dc5a749f1f25b0a7698dcb0b5ca7bf7a70be2a65
Praveen Bisht <prvnbist@gmail.com>
2020-06-02 00:43:32 +0530

    feat: devices/printers

--- entities/device/controllers.js
+++ entities/device/controllers.js
@@ -20,6 +20,16 @@ const UPDATE_COMPUTERS = `
    }
 `
 
+const UPDATE_PRINTERS = `
+   mutation createPrinters($objects: [deviceHub_printer_insert_input!]!, $update_columns: [deviceHub_printer_update_column!]!) {
+      createPrinters(objects: $objects, on_conflict: {constraint: printer_pkey, update_columns: $update_columns}) {
+      returning {
+            printNodeId
+         }
+      }
+   }
+`
+
 const base_url = 'https://api.printnode.com'
 
 export const process = async (req, res) => {
@@ -42,10 +52,51 @@ export const process = async (req, res) => {
             const { id, createTimestamp, ...rest } = computer
             return { printNodeId: id, ...rest }
          }),
-         update_columns: ['state']
+         update_columns: [
+            'name',
+            'inet',
+            'inet6',
+            'state',
+            'version',
+            'hostname'
+         ]
       })
 
-      return res.json({ data: computers })
+      const { data: printersList } = await axios.get(`${base_url}/printers`, {
+         auth: {
+            username: configs[0].value,
+            password: ''
+         }
+      })
+
+      const printers = await Promise.all(
+         printersList.map(async printer => {
+            const {
+               id,
+               name,
+               state,
+               computer,
+               description,
+               capabilities
+            } = printer
+
+            return {
+               name,
+               state,
+               description,
+               printNodeId: id,
+               computerId: computer.id,
+               ...(capabilities && { ...capabilities })
+            }
+         })
+      )
+
+      await client.request(UPDATE_PRINTERS, {
+         objects: printers,
+         update_columns: ['state', 'name', 'description']
+      })
+
+      return res.json({ data: { computers, printers } })
    } catch (error) {
       return res.json({ error: error.message })
    }
