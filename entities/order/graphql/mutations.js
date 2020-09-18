997ac7c12ea25664fc4886f03bd6d8bf44a5e9c3
Praveen Bisht <prvnbist@gmail.com>
2020-09-18 17:16:18 +0530

    fix: missing deliveryInfo response

--- entities/order/graphql/mutations.js
+++ entities/order/graphql/mutations.js
@@ -2,6 +2,7 @@ export const CREATE_ORDER = `
    mutation createOrder($object: order_order_insert_input!) {
       createOrder(object: $object) {
          id
+         deliveryInfo
       }
    }
 `
