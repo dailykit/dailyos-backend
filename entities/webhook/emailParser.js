b7d40e915d746996a3e6b009f3849b73be471773
Praveen Bisht <prvnbist@gmail.com>
2020-12-11 19:39:38 +0530

    feat: upsert third party order source

--- entities/webhook/emailParser.js
+++ entities/webhook/emailParser.js
@@ -1,15 +1,29 @@
 import { client } from '../../lib/graphql'
 
+const sources = {
+   swiggy: {
+      title: 'Swiggy',
+      imageUrl:
+         'https://s3.us-east-2.amazonaws.com/dailykit.org/third-party-orders/swiggy.png'
+   }
+}
+
 export const emailParser = async (req, res) => {
    try {
-      console.log(req.body)
       const { createThirdPartyOrder } = await client.request(
          CREATE_THIRD_PARTY_ORDER,
          {
             object: {
                parsedData: req.body,
                source: req.body.source,
-               thirdPartyOrderId: req.body.orderId
+               thirdPartyOrderId: req.body.orderId,
+               orderSource: {
+                  data: sources[req.body.source.toLowerCase()],
+                  on_conflict: {
+                     update_columns: 'imageUrl',
+                     constraint: 'thirdPartySource_title_key'
+                  }
+               }
             }
          }
       )
