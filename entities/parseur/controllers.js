45c19c8ba51b85b893bd4eb7680699aeb1da1bc8
Praveen Bisht <prvnbist@gmail.com>
2020-12-15 20:37:57 +0530

    feat: link mailbox id

--- entities/parseur/controllers.js
+++ entities/parseur/controllers.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { client } from '../../lib/graphql'
 
 const parseur = {
    insert: async (req, res) => {
@@ -8,16 +9,43 @@ const parseur = {
          if (!('id' in brand) && !brand.id)
             throw { message: 'Brand id is required!', status_code: 409 }
 
-         const { data = {} } = await axios({
+         const { status, data = {} } = await axios({
             method: 'POST',
             url: `${process.env.PLATFORM_URL}/api/parseur`,
             data: { brand, organization: { id: process.env.ORGANIZATION_ID } }
          })
+         if (status === 200 && 'success' in data && data.success) {
+            if ('data' in data && Object.keys(data.data).length > 0) {
+               try {
+                  await client.request(UPDATE_BRAND, {
+                     _set: {
+                        parseurMailBoxId: data.data.id
+                     }
+                  })
+               } catch (_) {
+                  throw {
+                     message: 'Failed to set parseur mailbox id!'
+                  }
+               }
+            }
+         }
          res.status(200).json({ success: true, data })
       } catch (error) {
-         res.status(500).json({ success: false, error })
+         const code =
+            'status_code' in error && error.status_code
+               ? error.status_code
+               : 500
+         res.status(code).json({ success: false, error })
       }
    }
 }
 
 export default parseur
+
+const UPDATE_BRAND = `
+   mutation updateBrand($id: Int!, $_set: brands_brand_set_input!) {
+      updateBrand(pk_columns: { id: $id }, _set: $_set) {
+         id
+      }
+   }
+`
