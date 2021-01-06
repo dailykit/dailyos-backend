f439dc85b0c554d9501efb5c1ac7abb86d3501e4
Praveen Bisht <prvnbist@gmail.com>
2021-01-06 16:46:40 +0530

    feat: set amount in cart

--- entities/order/controllers.js
+++ entities/order/controllers.js
@@ -279,7 +279,7 @@ export const take = async (req, res) => {
             deliveryPrice: cart.deliveryPrice,
             transactionId: cart.transactionId,
             currency: process.env.CURRENCY || '',
-            fulfillmentType: cart.fulfillmentInfo.type,
+            fulfillmentType: cart.fulfillmentInfo.type
          }
       })
 
@@ -325,7 +325,8 @@ export const take = async (req, res) => {
       await client.request(UPDATE_CART, {
          id,
          status: 'ORDER_PLACED',
-         orderId: Number(order.id)
+         orderId: Number(order.id),
+         amount: cart.totalPrice
       })
 
       if (Object.keys(cart.customerInfo).length > 0) {
