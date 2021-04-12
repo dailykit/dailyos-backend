94d2833443c73d014cce8487a7d1e2e120b78c1d
Praveen Bisht <prvnbist@gmail.com>
2021-04-12 22:15:07 +0530

    feat: update manage occurence
    - create entry for missing occurence customer
    - mark isPaused, isSkipped for paused customer
    - attempt payment on valid carts
    - skip occurence customer for invalid carts

--- entities/occurence/controllers/manageOccurence.js
+++ entities/occurence/controllers/manageOccurence.js
@@ -1,5 +1,10 @@
 import { client } from '../../../lib/graphql'
-import { UPDATE_CARTS, UPDATE_OCCURENCE_CUSTOMER } from '../graphql'
+import {
+   CUSTOMERS,
+   INSERT_OCCURENCE_CUSTOMER,
+   UDPATE_OCCURENCE_CUSTOMER_CARTS,
+   UPDATE_OCCURENCE_CUSTOMER_BY_PK
+} from '../graphql'
 
 export const manageOccurence = async (req, res) => {
    try {
@@ -17,18 +22,142 @@ export const manageOccurence = async (req, res) => {
          node.cutoffTimeStamp = cutoffTimeStamp
       }
 
-      await client.request(UPDATE_OCCURENCE_CUSTOMER, {
-         subscriptionOccurenceId: { _eq: node.id },
-         cutoffTimeStamp: { _eq: node.cutoffTimeStamp },
-         _set: {
-            isSkipped: true
+      // CREATE OCCURENCE CUSTOMER
+      const {
+         subscription_view_full_occurence_report: pendingCustomers = []
+      } = await client.request(CUSTOMERS, {
+         where: {
+            keycloakId: { _is_null: true },
+            subscriptionOccurenceId: { _eq: id }
          }
       })
 
-      await client.request(UPDATE_CARTS, {
-         subscriptionOccurenceId: { _eq: node.id },
-         cutoffTimeStamp: { _eq: node.cutoffTimeStamp }
+      if (pendingCustomers.length > 0) {
+         const rows = await Promise.all(
+            pendingCustomers
+               .filter(node => node.keycloakId)
+               .map(node => {
+                  return {
+                     brand_customerId: node.brand_customerId,
+                     keycloakId: node.brandCustomer.keycloakId,
+                     subscriptionOccurenceId: node.subscriptionOccurenceId
+                  }
+               })
+         )
+         await client.request(INSERT_OCCURENCE_CUSTOMER, {
+            objects: rows
+         })
+      }
+
+      // MARK OCCURENCE CUSTOMER PAUSED IF CUSTOMER HAS PAUSED
+      /*
+         mark isPaused=true, isSkipped=true
+      */
+      const {
+         subscription_view_full_occurence_report: pausedCustomers = []
+      } = await client.request(CUSTOMERS, {
+         where: {
+            betweenPause: { _eq: true },
+            subscriptionOccurenceId: { _eq: id }
+         }
       })
+
+      if (pausedCustomers.length > 0) {
+         const rows = await Promise.all(
+            pausedCustomers.map(node => {
+               return {
+                  keycloakId: node.keycloakId,
+                  brand_customerId: node.brand_customerId,
+                  subscriptionOccurenceId: node.subscriptionOccurenceId
+               }
+            })
+         )
+
+         await Promise.all(
+            rows.map(async row => {
+               try {
+                  const {
+                     update_subscription_subscriptionOccurence_customer_by_pk = {}
+                  } = await client.request(UPDATE_OCCURENCE_CUSTOMER_BY_PK, {
+                     pk_columns: {
+                        keycloakId: row.keycloakId,
+                        brand_customerId: row.brand_customerId,
+                        subscriptionOccurenceId: row.subscriptionOccurenceId
+                     },
+                     _set: { isPaused: true, isSkipped: true }
+                  })
+                  return update_subscription_subscriptionOccurence_customer_by_pk
+               } catch (error) {
+                  return error
+               }
+            })
+         )
+      }
+
+      // UPDATE CART TO ATTEMPT PAYMENT
+
+      /*
+         update subscriptionOccurence & subscriptionOccurence_customer log column at each mutation
+      */
+      const {
+         subscription_view_full_occurence_report: validCustomers = []
+      } = await client.request(CUSTOMERS, {
+         where: {
+            isPaused: { _eq: false },
+            isSkipped: { _eq: false },
+            cartId: { _is_null: false },
+            paymentStatus: { _neq: 'SUCCEEDED' },
+            subscriptionOccurenceId: { _eq: id }
+         }
+      })
+
+      if (validCustomers.length > 0) {
+         // PROCESS VALID ITEM COUNT
+         const validCarts = validCustomers.filter(node => node.isItemCountValid)
+         if (validCarts.length > 0) {
+            await client.request(UDPATE_OCCURENCE_CUSTOMER_CARTS, {
+               where: {
+                  id: { _in: validCarts.map(node => node.cartId) }
+               },
+               _inc: { paymentRetryAttempt: 1 }
+            })
+         }
+
+         // SKIP INVALID ITEM COUNT
+         const invalidCarts = validCustomers.filter(
+            node => !node.isItemCountValid
+         )
+         if (invalidCarts.length > 0) {
+            const rows = await Promise.all(
+               invalidCarts.map(node => {
+                  return {
+                     keycloakId: node.keycloakId,
+                     brand_customerId: node.brand_customerId,
+                     subscriptionOccurenceId: node.subscriptionOccurenceId
+                  }
+               })
+            )
+            await Promise.all(
+               rows.map(async row => {
+                  try {
+                     const {
+                        update_subscription_subscriptionOccurence_customer_by_pk = {}
+                     } = await client.request(UPDATE_OCCURENCE_CUSTOMER_BY_PK, {
+                        pk_columns: {
+                           keycloakId: row.keycloakId,
+                           brand_customerId: row.brand_customerId,
+                           subscriptionOccurenceId: row.subscriptionOccurenceId
+                        },
+                        _set: { isSkipped: true }
+                     })
+                     return update_subscription_subscriptionOccurence_customer_by_pk
+                  } catch (error) {
+                     return error
+                  }
+               })
+            )
+         }
+      }
 
       return res.status(200).json({
          success: true,
--- entities/occurence/graphql/queries.js
+++ entities/occurence/graphql/queries.js
@@ -36,3 +36,21 @@ query customerDetails($id: Int!) {
   }
 }
 `
+
+export const CUSTOMERS = `
+   query subscription_view_full_occurence_report(
+      $where: subscription_view_full_occurence_report_bool_exp!
+   ) {
+      subscription_view_full_occurence_report(where: $where) {
+         cartId
+         keycloakId
+         brand_customerId
+         isItemCountValid
+         subscriptionOccurenceId
+         brandCustomer {
+            id
+            keycloakId
+         }
+      }
+   }
+`
