b2dadba44680381f919b2c37d6abbb9cabe68be9
Praveen Bisht <prvnbist@gmail.com>
2021-04-29 18:59:34 +0530

    fix: more validations

--- utils/reminderEmail/addProductsToCart.js
+++ utils/reminderEmail/addProductsToCart.js
@@ -14,36 +14,40 @@ export const addProductsToCart = async ({
          subscriptionOccurenceId,
          message: 'Adding Products To Cart'
       })
+      const { brandCustomers = [] } = await client.request(
+         PENDING_PRODUCT_COUNT,
+         {
+            subscriptionOccurenceId,
+            brandCustomerId: brand_customerId
+         }
+      )
+
+      if (brandCustomers.lentgh === 0) return
+      const [brand_customer] = brandCustomers
+      const { subscriptionOccurences = [] } = brand_customer
+
+      if (subscriptionOccurences.length === 0) return
       const {
-         brandCustomers: [
-            {
-               subscriptionOccurences: [
-                  {
-                     validStatus = {},
-                     cartId,
-                     isSkipped,
-                     subscriptionOccurence = {}
-                  }
-               ] = []
-            }
-         ] = []
-      } = await client.request(PENDING_PRODUCT_COUNT, {
-         brandCustomerId,
-         subscriptionOccurenceId
-      })
+         validStatus = {},
+         cartId = null,
+         isSkipped,
+         subscriptionOccurence = {}
+      } = subscriptionOccurences[0]
+
       const method = require(`../../options/${
          subscriptionOccurence.subscriptionAutoSelectOption &&
          subscriptionOccurence.subscriptionAutoSelectOption
       }`)
 
       const sortedProducts = await method.default(products)
-      let i = 0,
-         count = validStatus.pendingProductsCount
+      let i = 0
+      let count = validStatus.pendingProductsCount
 
-      if (sortedProducts.length >= count) {
+      if (Array.isArray(sortedProducts) && sortedProducts.length >= count) {
          while (i < count) {
             try {
-               let node = insertCartId(...sortedProducts[i].cartItem, cartId)
+               let { cartItem = {} } = sortedProducts[i]
+               let node = insertCartId(cartItem, cartId)
                await client.request(INSERT_CART_ITEM, {
                   object: {
                      node,
@@ -139,18 +143,27 @@ export const INSERT_CART_ITEM = `
 `
 
 const PENDING_PRODUCT_COUNT = `
-query pendingProductCount($brandCustomerId: Int!, $subscriptionOccurenceId: Int!) {
-  brandCustomers(where: {id: {_eq: $brandCustomerId}}) {
-    subscriptionOccurences(where: {subscriptionOccurenceId: {_eq: $subscriptionOccurenceId}}) {
-      validStatus
-      cartId
-      isSkipped
-      subscriptionOccurence {
-        subscriptionAutoSelectOption
-        subscriptionId
+   query pendingProductCount(
+      $brandCustomerId: Int!
+      $subscriptionOccurenceId: Int!
+   ) {
+      brandCustomers(where: { id: { _eq: $brandCustomerId } }) {
+         id
+         subscriptionOccurences(
+            where: {
+               subscriptionOccurenceId: { _eq: $subscriptionOccurenceId }
+            }
+         ) {
+            validStatus
+            cartId
+            isSkipped
+            subscriptionOccurenceId
+            subscriptionOccurence {
+               id
+               subscriptionAutoSelectOption
+               subscriptionId
+            }
+         }
       }
-    }
-  }
-}
-
+   }
 `
--- utils/reminderEmail/sendEmail.js
+++ utils/reminderEmail/sendEmail.js
@@ -8,31 +8,40 @@ export const sendEmail = async ({
    fileName
 }) => {
    try {
-      const {
-         subscriptionOccurences: [
-            {
-               subscription: {
-                  identifier: { template },
-                  brand_customers = []
-               } = {}
-            }
-         ] = []
-      } = await client.request(GET_CUSTOMER_EMAIL, {
-         subscriptionOccurenceId,
-         brandCustomerId
-      })
+      const { subscriptionOccurences = [] } = await client.request(
+         GET_CUSTOMER_EMAIL,
+         {
+            subscriptionOccurenceId,
+            brandCustomerId
+         }
+      )
+
+      if (subscriptionOccurences.length === 0) return
+      const [occurence] = subscriptionOccurences
+
+      const { subscription = {} } = occurence
+      const { reminderSettings = {} } = subscription
+
+      if (
+         !('template' in reminderSettings) ||
+         Object.keys(reminderSettings.template || {}).length === 0
+      )
+         return
 
       const {
-         brands_brand_subscriptionStoreSetting: [{ templateSettings = {} }] = []
+         brands_brand_subscriptionStoreSetting = []
       } = await client.request(GET_TEMPLATE_SETTINGS, {
          identifier: template
       })
 
-      let html = await getHtml(templateSettings.template, {
+      if (brands_brand_subscriptionStoreSetting.length === 0) return
+      const { value } = brands_brand_subscriptionStoreSetting[0]
+
+      let html = await getHtml(value.template, {
          data: {
-            brand_customerId: brandCustomerId,
+            fileName,
             subscriptionOccurenceId,
-            fileName
+            brand_customerId: brandCustomerId
          }
       })
 
@@ -51,24 +60,34 @@ export const sendEmail = async ({
    }
 }
 
-const GET_CUSTOMER_EMAIL = `query customerEmail($subscriptionOccurenceId: Int!, $brandCustomerId: Int!) {
-  subscriptionOccurences(where: {id: {_eq: $subscriptionOccurenceId}}) {
-    subscription {
-      identifier: reminderSettings
-      brand_customers(where: {id: {_eq: $brandCustomerId}}) {
-        customer {
-          id
-          email
-        }
+const GET_CUSTOMER_EMAIL = `
+   query subscriptionOccurences($subscriptionOccurenceId: Int!, $brandCustomerId: Int!) {
+      subscriptionOccurences(where: { id: { _eq: $subscriptionOccurenceId } }) {
+         id
+         subscriptionId
+         subscription {
+            id
+            reminderSettings
+            brand_customers(where: { id: { _eq: $brandCustomerId } }) {
+               id
+               customer {
+                  id
+                  email
+               }
+            }
+         }
       }
-    }
-  }
-}`
+   }
+`
 
 export const GET_TEMPLATE_SETTINGS = `
-    query getTemplateSettings($identifier: String! ) {
-        brands_brand_subscriptionStoreSetting(where: {subscriptionStoreSetting: {identifier: {_eq: $identifier} }}) {
-            templateSettings: value
-        }
-    }   
+   query brands_brand_subscriptionStoreSetting($identifier: String!) {
+      brands_brand_subscriptionStoreSetting(
+         where: {
+            subscriptionStoreSetting: { identifier: { _eq: $identifier } }
+         }
+      ) {
+         value
+      }
+   }
 `
